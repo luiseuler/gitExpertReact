ab290df80033dfca743b99e72eaee37d9253796a
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-07-05 23:36:10 -0600

    Agregar una nueva categoría

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -3,19 +3,24 @@ import { useState } from 'react';
 export const GiftExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
+  const onAddCategory = () => {
+    setCategories([...categories, 'Valorant']);
+  }
+
   return (
     <>
       {/* titulo */}
       <h1>GiftExpertApp</h1>
 
       {/* Input */}
+      <button onClick={onAddCategory}>Agregar</button>
 
       {/* Listado de gift */}
       <ol>
         {/* Gift Item */}
-        { categories.map(category => {
+        {categories.map(category => {
           return <li key={category}>{category}</li>
-        }) }
+        })}
       </ol>
 
     </>
