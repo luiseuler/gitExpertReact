c353c0a214d82184ba6266206b0821b37cd2d19e
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-07-05 23:17:08 -0600

    GifExpertApp - Component

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,7 +1,14 @@
 export const GiftExpertApp = () => {
   return (
     <>
+      {/* titulo */}
       <h1>GiftExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de gift */}
+
+        {/* Gift Item */}
     </>
   );
 }
