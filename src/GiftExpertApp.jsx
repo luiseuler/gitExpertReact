86f287f489a131eed7a710d61cdea4d6c1c0cd64
Luis Alejandro Garcia Garcia <luisalejandrogarciax2@gmail.com>
2023-07-08 21:49:41 -0600

    Archivos de barril

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
-
-import { AddCategory } from './components/AddCategory';
-import { GiftGrid } from './components/GiftGrid';
+import { AddCategory, GiftGrid} from './components';
 
 export const GiftExpertApp = () => {
   const [categories, setCategories] = useState(['Dragon Ball']);
